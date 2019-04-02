13d2502b225a6d6e951a0b02f4fb4eb941f3a467
Rafael Batista <maranata_rafa@hotmail.com>
2019-04-01 21:31:31 -0400

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace C_Sharp_Intermediario
+{
+    partial class frm_Agenda
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Titulo = new System.Windows.Forms.Label();
+            this.lbx_Contato = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbl_Titulo
+            // 
+            this.lbl_Titulo.AutoSize = true;
+            this.lbl_Titulo.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Titulo.Location = new System.Drawing.Point(12, 9);
+            this.lbl_Titulo.Name = "lbl_Titulo";
+            this.lbl_Titulo.Size = new System.Drawing.Size(73, 37);
+            this.lbl_Titulo.TabIndex = 0;
+            this.lbl_Titulo.Text = "Lista";
+            // 
+            // lbx_Contato
+            // 
+            this.lbx_Contato.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbx_Contato.FormattingEnabled = true;
+            this.lbx_Contato.ItemHeight = 30;
+            this.lbx_Contato.Location = new System.Drawing.Point(19, 50);
+            this.lbx_Contato.Name = "lbx_Contato";
+            this.lbx_Contato.Size = new System.Drawing.Size(427, 214);
+            this.lbx_Contato.TabIndex = 1;
+            // 
+            // frm_Agenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 293);
+            this.Controls.Add(this.lbx_Contato);
+            this.Controls.Add(this.lbl_Titulo);
+            this.Name = "frm_Agenda";
+            this.Text = "Agenda";
+            this.Load += new System.EventHandler(this.frm_Agenda_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Titulo;
+        private System.Windows.Forms.ListBox lbx_Contato;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace C_Sharp_Intermediario
+{
+    public partial class frm_Agenda : Form
+    {
+        public frm_Agenda()
+        {
+            InitializeComponent();
+        }
+        private void frm_Agenda_Load(object sender, EventArgs e)
+        {
+            CriarContato();
+            lbl_Titulo.Text = CarregarTitulo();
+            CarregarContatos();
+        }
+        private string CarregarTitulo()
+        {
+            //XmlDocument: Classe para manipulação de arquivos XML.
+            XmlDocument docXML = new XmlDocument();
+            //Carrega o arquivo com os nós e seus respectivos dados.
+            docXML.Load(@"C:\Users\Vostro 5480\OneDrive\DEV\RepositGit\XML_Aula\Agenda.xml");
+
+            //SelectSingleNode: retorna apenas um nó ou, no caso de mais de um, retorna o primeiro.
+            XmlNode nohTitulo = docXML.SelectSingleNode("//titulo");
+
+            return nohTitulo.InnerText;
+        }
+        //Carrega lista de contatos.
+        private void CarregarContatos()
+        {
+            XmlDocument docXML = new XmlDocument();
+            docXML.Load(@"C:\Users\Vostro 5480\OneDrive\DEV\RepositGit\XML_Aula\Agenda.xml");
+            
+            //XMLNodeList: Carrega lista de nós. | SelectNodes: retorna todos os nós existentes no xPath.
+            XmlNodeList contatos = docXML.SelectNodes("//contato");
+
+            //XMLNode: Carrega apenas um nó por iteração.
+            foreach (XmlNode contato in contatos)
+            {
+                //Attributes: Captura o atributo do nó.
+                string id = contato.Attributes["id"].Value;
+                string nome = contato.Attributes["nome"].Value;
+                string telefone = contato.Attributes["telefone"].Value;
+                //formatação dos itens.
+                string representacaoContato = nome + ", " + telefone + ", " + id;
+                //Insere os itens na lista.
+                lbx_Contato.Items.Add(representacaoContato);
+            }
+        }
+        private void CriarContato()
+        {
+            XmlDocument docXML = new XmlDocument();
+            docXML.Load(@"C:\Users\Vostro 5480\OneDrive\DEV\RepositGit\XML_Aula\Agenda.xml");
+            
+            //Criar os atributos da tag contato.
+            XmlAttribute atribId = docXML.CreateAttribute("id");
+            atribId.Value = "5";
+            XmlAttribute atribNome = docXML.CreateAttribute("nome");
+            atribNome.Value = "teste novo elemento";
+            XmlAttribute atribTelefone = docXML.CreateAttribute("telefone");
+            atribTelefone.Value = "67 99995555";
+
+            //Cria uma nova (tag) contato e adiciona os respetivos atributos.
+            XmlNode novoContato = docXML.CreateElement("contato");
+            novoContato.Attributes.Append(atribId);
+            novoContato.Attributes.Append(atribNome);
+            novoContato.Attributes.Append(atribTelefone);
+
+            //Adiciona (Vincula) a nova tag ao nó contatos.
+            XmlNode contatos = docXML.SelectSingleNode("//contatos");
+            contatos.AppendChild(novoContato);
+
+            //Salva um novo arquivo sobrescrevendo o anterior.
+            docXML.Save(@"C:\Users\Vostro 5480\OneDrive\DEV\RepositGit\XML_Aula\Agenda.xml");
+        }
+    }
+}
